526ae70eea8ad43b21b1a69e4aa9d540f4b42f72
Steven Yu <sky3947@rit.edu>
2024-08-27 14:59:57 -0400

    initial functionality

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+let input = [];
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key.toLowerCase();
+
+    if (letters.includes(key)) {
+        input.push(key);
+    } else if (letters === 'backspace') {
+        input.pop();
+    } else if (key === 'enter') {
+        // Todo: cycle color.
+    }
+});
+
 // document.getElementById('list').textContent = `test`;
